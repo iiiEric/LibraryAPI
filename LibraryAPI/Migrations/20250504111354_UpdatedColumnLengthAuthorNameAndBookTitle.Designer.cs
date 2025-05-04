745f4406ab326e544973d056bd0a3d16e0ded40d
iiiEric <ericcampos1997@gmail.com>
2025-05-04 13:16:30 +0200

    fix(db): Updated column lengths for AuthorName and BookTitle.

--- LibraryAPI/Migrations/20250504111354_UpdatedColumnLengthAuthorNameAndBookTitle.Designer.cs
+++ LibraryAPI/Migrations/20250504111354_UpdatedColumnLengthAuthorNameAndBookTitle.Designer.cs
@@ -3,6 +3,7 @@ using LibraryAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LibraryAPI.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250504111354_UpdatedColumnLengthAuthorNameAndBookTitle")]
+    partial class UpdatedColumnLengthAuthorNameAndBookTitle
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
